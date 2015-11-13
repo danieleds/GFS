380f1490cfea7b1b066d89e2be30c8ede840d46e
Daniele Di Sarli <danieleds0@gmail.com>
2015-11-13 16:00:22 +0100

    Test GhostFile

--- semanticfs/tests/functional.py
+++ semanticfs/tests/functional.py
@@ -26,16 +26,47 @@ class FunctionalTests(unittest.TestCase):
         shutil.rmtree(self._dspath)
         shutil.rmtree(self._fspath)
 
-    def testMkdir(self):
-        os.mkdir(os.path.join(self._fspath, 'standardDir'))
-        os.mkdir(os.path.join(self._fspath, '_semanticDir'))
-        os.mkdir(os.path.join(self._fspath, '_semanticDir', '_tag1'))
-        os.mkdir(os.path.join(self._fspath, '_semanticDir', '_tag1', 'taggedDir'))
+    def _path(self, *paths):
+        return os.path.join(self._dspath, *paths)
 
-        self.assertRaises(FileExistsError, os.mkdir, os.path.join(self._fspath, 'standardDir'))
-        self.assertRaises(FileExistsError, os.mkdir, os.path.join(self._fspath, '_semanticDir'))
-        self.assertRaises(FileExistsError, os.mkdir, os.path.join(self._fspath, '_semanticDir', '_tag1'))
-        self.assertRaises(FileExistsError, os.mkdir, os.path.join(self._fspath, '_semanticDir', '_tag1', 'taggedDir'))
+    def testMkdir(self):
+        os.mkdir(self._path('standardDir'))
+        os.mkdir(self._path('_semanticDir'))
+        os.mkdir(self._path('_semanticDir', '_tag1'))
+        os.mkdir(self._path('_semanticDir', '_tag1', 'taggedDir'))
+
+        self.assertRaises(FileExistsError, os.mkdir, self._path('standardDir'))
+        self.assertRaises(FileExistsError, os.mkdir, self._path('_semanticDir'))
+        self.assertRaises(FileExistsError, os.mkdir, self._path('_semanticDir', '_tag1'))
+        self.assertRaises(FileExistsError, os.mkdir, self._path('_semanticDir', '_tag1', 'taggedDir'))
+
+    def testGhostFile(self):
+        os.mkdir(self._path('_sem'))
+        os.mkdir(self._path('_sem', '_t1'))
+        os.mkdir(self._path('_sem', '_t2'))
+
+        goldcontent = b"HelloWorld" * 100000  # 1000kB
+
+        with open(self._path('_sem', 'x'), 'wb') as f:
+            f.write(goldcontent)
+
+        shutil.copy2(self._path('_sem', 'x'), self._path('_sem', '_t1', 'x'))
+
+        with open(self._path('_sem', 'x'), 'rb') as f:
+            self.assertEqual(f.read(), goldcontent)
+
+        with open(self._path('_sem', '_t1', 'x'), 'rb') as f:
+            self.assertEqual(f.read(), goldcontent)
+
+        shutil.copy2(self._path('_sem', 'x'), self._path('_sem', '_t1', 'x'))
+
+        with open(self._path('_sem', '_t1', 'x'), 'rb') as f:
+            self.assertEqual(f.read(), goldcontent)
+
+        shutil.copy2(self._path('_sem', '_t1', 'x'), self._path('_sem', '_t2', 'x'))
+
+        with open(self._path('_sem', '_t2', 'x'), 'rb') as f:
+            self.assertEqual(f.read(), goldcontent)
 
 
 def main():
