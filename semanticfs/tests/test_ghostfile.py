ff12debca1d75142868da7153d4143006039bb08
Daniele Di Sarli <danieleds0@gmail.com>
2015-11-15 19:22:06 +0100

    GhostFile unit tests

---
+++ semanticfs/tests/test_ghostfile.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import unittest
+import tempfile
+import shutil
+import os
+from semanticfs.ghostfile import GhostFile
+
+
+class PathInfoTests(unittest.TestCase):
+
+    def setUp(self):
+        self._dspath = tempfile.mkdtemp()
+        self._datapath = os.path.join(self._dspath, "x")
+
+    def tearDown(self):
+        shutil.rmtree(self._dspath)
+
+    def _fillfile(self, content):
+        with open(self._datapath, 'wb') as f:
+            f.write(content)
+
+    def test_missing_file(self):
+        self.assertRaises(FileNotFoundError, GhostFile, self._datapath, None)
+
+    def test_empty_read(self):
+        self._fillfile(b"")
+        ghost = GhostFile(self._datapath, None)
+        with open(self._datapath, 'rb') as f:
+            self.assertEqual(ghost.read(10, 0, f.fileno()), b"")
+        ghost.release()
+
+    def test_read(self):
+        self._fillfile(b"0123456789")
+        ghost = GhostFile(self._datapath, None)
+
+        with open(self._datapath, 'rb') as f:
+            self.assertEqual(ghost.read(10, 0, f.fileno()), b"0123456789")
+        with open(self._datapath, 'rb') as f:
+            self.assertEqual(ghost.read(5, 5, f.fileno()), b"56789")
+        with open(self._datapath, 'rb') as f:
+            self.assertEqual(ghost.read(5, 8, f.fileno()), b"89")
+        with open(self._datapath, 'rb') as f:
+            self.assertEqual(ghost.read(5, 10, f.fileno()), b"")
+
+        with open(self._datapath, 'r+b') as f:
+            ghost.apply(f.fileno())
+        ghost.release()
+
+        # Make sure the file was not modified
+        with open(self._datapath, 'rb') as f:
+            self.assertEqual(f.read(), b"0123456789")
+
+    def test_write_same(self):
+        self._fillfile(b"0123456789")
+        ghost = GhostFile(self._datapath, None)
+
+        with open(self._datapath, 'r+b') as f:
+            ghost.write(b"01234", 0, f.fileno())
+            self.assertEqual(ghost.read(100, 0, f.fileno()), b"0123456789")
+
+        with open(self._datapath, 'r+b') as f:
+            ghost.write(b"789", 7, f.fileno())
+            self.assertEqual(ghost.read(100, 0, f.fileno()), b"0123456789")
+
+        with open(self._datapath, 'r+b') as f:
+            ghost.write(b"3456", 3, f.fileno())
+            self.assertEqual(ghost.read(100, 0, f.fileno()), b"0123456789")
+
+        with open(self._datapath, 'r+b') as f:
+            ghost.apply(f.fileno())
+        ghost.release()
+
+        # Make sure the file was not modified
+        with open(self._datapath, 'rb') as f:
+            self.assertEqual(f.read(), b"0123456789")
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
