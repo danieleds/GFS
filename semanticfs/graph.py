22cc56d337e1619666db376ec3d83ba4fc1e4c35
Daniele Di Sarli <danieleds0@gmail.com>
2015-11-20 14:21:01 +0100

    Remove fixme

--- semanticfs/graph.py
+++ semanticfs/graph.py
@@ -98,7 +98,6 @@ class Graph:
         self._adjacency_in[to_node].remove(from_node)
 
     def incoming_arcs(self, node) -> list:
-        # FIXME Is this method (and self._adjacency_in) really necessary?
         if not self.has_node(node):
             raise ValueError('Node is missing')
 
